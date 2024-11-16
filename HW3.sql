1cae79380850f258afd60ed75ce94607d230c1ca
Ali-AliAli-Ali <alivolf@icloud.com>
2024-11-16 17:53:58 +0300

    Done 5th, 6th parts of HW3

--- HW3.sql
+++ HW3.sql
@@ -83,3 +83,72 @@ UNION
 )
 ORDER BY sale_month, total_sales DESC;
 
+
+-- 5
+
+
+INSERT INTO sales (employee_id, product_id, quantity, sale_date)
+VALUES
+    (4, 4, 25, '2024-11-13'),
+    (2, 3, 13, '2024-11-14'),
+    (3, 3, 20, '2024-11-14'),
+    (3, 2, 5, '2024-11-14'),
+    (4, 1, 8, '2024-11-14'),
+    (2, 1, 2, '2024-11-14'),
+    (2, 4, 5, '2024-11-15'),
+    (3, 2, 15, '2024-11-15'),
+    (4, 1, 18, '2024-11-15'),
+    (2, 1, 22, '2024-11-15'),
+    (2, 4, 35, '2024-11-15'),
+    (1, 1, 2, '2024-11-15'),
+    (2, 1, 10, '2024-11-15'),
+    (3, 4, 3, '2024-11-15'),
+    (3, 2, 5, '2024-11-15'),
+    (4, 1, 8, '2024-11-15'),
+    (2, 4, 20, '2024-11-15'),
+    (2, 4, 5, '2024-11-15'),
+    (3, 2, 15, '2024-11-15'),
+    (4, 1, 18, '2024-11-15'),
+    (2, 1, 22, '2024-11-15'),
+    (2, 4, 35, '2024-11-15');
+
+
+EXPLAIN ANALYZE
+SELECT product_id, SUM(quantity) AS total_sales FROM sales
+    GROUP BY product_id
+    ORDER BY product_id
+LIMIT 20;
+
+-- Planning Time: 0.157 ms
+-- Execution Time: 0.162 ms
+
+CREATE INDEX idx_department ON employees(department);
+CREATE INDEX idx_sale_date ON sales(sale_date);
+
+EXPLAIN ANALYZE
+SELECT product_id, SUM(quantity) AS total_sales FROM sales
+    GROUP BY product_id
+    ORDER BY product_id
+LIMIT 20;
+
+-- Planning Time: 1.651 ms
+-- Execution Time: 0.090 ms
+
+-- DROP INDEX idx_department, idx_sale_date;
+
+
+-- 6
+
+
+EXPLAIN
+SELECT product_id, SUM(quantity) AS total_sales FROM sales
+    GROUP BY product_id
+    ORDER BY product_id
+LIMIT 20;
+
+-- Limit  (cost=2.25..2.60 rows=20 width=12)
+--   ->  GroupAggregate  (cost=2.25..2.86 rows=35 width=12)
+--       Group Key: product_id
+--         ->  Sort  (cost=2.25..2.34 rows=35 width=8)
+--             Sort Key: product_id
+--               ->  Seq Scan on sales  (cost=0.00..1.35 rows=35 width=8)
