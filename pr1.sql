ac03f7eb6b52a31793a4f78261e9f68fcf88351f
Ali-AliAli-Ali <alivolf@icloud.com>
2024-12-05 19:41:45 +0300

    Added trigger

--- pr1.sql
+++ pr1.sql
@@ -10,7 +10,6 @@ CREATE TABLE cameras (
     has_infr BOOLEAN DEFAULT false,
     is_eol   BOOLEAN NOT NULL DEFAULT false
 );
-ALTER TABLE cameras SET 
 
 
 CREATE TABLE ros_versions (
@@ -66,12 +65,12 @@ BEGIN
 		WHEN NEW.name LIKE 'SR3%' THEN NEW.series = 'SR300';
 		WHEN NEW.name LIKE 'T2%'  THEN NEW.series = 'T200';
 	END CASE;
-    RAISE NOTICE 'Series is set to %.', NEW.series;
+    RAISE NOTICE 'The camera % series is set to %.', NEW.name, NEW.series;
 	RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER set_series_trigger
+CREATE OR REPLACE TRIGGER set_series_trigger
     BEFORE INSERT OR UPDATE ON cameras
     FOR EACH ROW 
 EXECUTE FUNCTION set_series();
@@ -81,16 +80,16 @@ CREATE OR REPLACE FUNCTION check_type()
 RETURNS TRIGGER AS $$
 BEGIN
 	IF NOT NEW.type = ANY(
-        '{"Depth Camera", "LiDAR Camera", "Tracking Camera", "Front Camera"}'
+        '{"Depth Camera", "LiDAR Camera", "Tracking Camera"}'
     ) THEN
-        RAISE WARNING 'The camera type may be written wrong.' 
+        RAISE WARNING 'The camera % type may be written wrong.', NEW.name 
         USING HINT = 'Please check your spelling and correct the camera type if needed.';
     END IF;
 	RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER check_type_trigger
+CREATE OR REPLACE TRIGGER check_type_trigger
     BEFORE INSERT OR UPDATE ON cameras
     FOR EACH ROW 
 EXECUTE FUNCTION check_type();
@@ -100,14 +99,14 @@ CREATE OR REPLACE FUNCTION set_has_imu()
 RETURNS TRIGGER AS $$
 BEGIN
 	IF NEW.name LIKE '%i%' THEN
-        RAISE NOTICE 'The camera seems to have an IMU. The according field is set to true';
+        RAISE NOTICE 'The camera % seems to have an IMU. The according field is set to true', NEW.name;
         NEW.has_imu = true;
     END IF;
 	RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER set_has_imu_trigger
+CREATE OR REPLACE TRIGGER set_has_imu_trigger
     BEFORE INSERT OR UPDATE ON cameras
     FOR EACH ROW 
 EXECUTE FUNCTION set_has_imu();
@@ -117,14 +116,34 @@ CREATE OR REPLACE FUNCTION set_has_infr()
 RETURNS TRIGGER AS $$
 BEGIN
 	IF NEW.name LIKE '%f%' THEN
-        RAISE NOTICE 'The camera seems to have an IR-Pass filter. The according field is set to true';
+        RAISE NOTICE 'The camera % seems to have an IR-Pass filter. The according field is set to true', NEW.name;
         NEW.has_infr = true;
     END IF;
 	RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER set_has_infr_trigger
+CREATE OR REPLACE TRIGGER set_has_infr_trigger
     BEFORE INSERT OR UPDATE ON cameras
     FOR EACH ROW 
 EXECUTE FUNCTION set_has_infr();
+
+
+CREATE OR REPLACE FUNCTION check_ubuntu()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NOT NEW.ubu_version = ANY(
+        '{"14.04", "16.04", "18.04", "20.04", "22.04", "24.04", "24.10"}'
+    ) THEN
+        RAISE EXCEPTION 'The Ubuntu version of % is invalid.', NEW.name
+        USING HINT = 'Please enter 2 numbers (with point as a delimeter) representing the Ubuntu version.';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER check_ubuntu_trigger
+    BEFORE INSERT OR UPDATE ON ros_versions
+    FOR EACH ROW
+EXECUTE FUNCTION check_ubuntu();
+
